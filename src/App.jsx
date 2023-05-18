dd868947cdf89de9fd75f118fb40f81bb74867c7
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-05-18 19:30:29 +0300

    Basic swiper using Swiper.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from './components/About/About';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
 import Cars from './components/Cars/Cars';
+import MySwiper from './components/Cars/CarsSlider/Swiper';
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<About />
 			<Services />
 			<Cars />
+			<MySwiper />
 		</>
 	);
 }
---
+++ src/components/Cars/CarsSlider/Swiper.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper';
+
+import classes from './CarsSlider.module.css';
+
+export default function MySwiper() {
+	return (
+		<>
+			<Swiper
+				className={classes.swiper}
+				modules={[Pagination, Navigation]}
+				navigation
+				slidesPerView={3}
+				spaceBetween={30}
+				centeredSlides={true}
+				pagination={{
+					clickable: true,
+				}}>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+		</>
+	);
+}
